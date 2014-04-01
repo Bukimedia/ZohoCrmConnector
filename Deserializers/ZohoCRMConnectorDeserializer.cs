6064b98fa6019973bd7a07b39f85c44e0d37c01d
marcos-torrente <marcos.torrente@bukimedia.com>
2014-04-01 14:56:32 +0200

    1-4-14

---
+++ Deserializers/ZohoCRMConnectorDeserializer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace ZohoCrmConnector.Deserializers
+{
+    class ZohoCRMConnectorDeserializer
+    {
+        public List<string> deserializer(XmlDocument xDoc, String tipo)
+        {
+            List<string> lista = new List<string>();
+            XmlNodeList respuesta = xDoc.GetElementsByTagName("response");
+            XmlNodeList resultado = ((XmlElement)respuesta[0]).GetElementsByTagName("result");
+
+            XmlNodeList leads = ((XmlElement)resultado[0]).GetElementsByTagName(tipo);
+
+            XmlNodeList columna = ((XmlElement)leads[0]).GetElementsByTagName("row");
+            XmlNodeList elemento = ((XmlElement)leads[0]).GetElementsByTagName("FL");
+
+            foreach (XmlElement nodo in elemento)
+            {
+                lista.Add(nodo.InnerText);
+            }
+            return lista;
+        }
+    }
+}
+
+/*
+        public long leadID { get; set; }
+        public long smowerID { get; set; }
+        public long leadOwner { get; set; }
+        public string company { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        //public string designation { get; set; }
+        public string tittle { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string fax { get; set; }
+        public string mobile { get; set; }
+        public string website { get; set; }
+        public long campaingSource { get; set; }
+        //public string leadSource { get; set; }
+        public string leadStatus { get; set; }
+        public string industry { get; set; }
+        public int numEmployees { get; set; }
+        public double annualRevenue { get; set; }
+        public string rating { get; set; }
+        public long smcreatorID { get; set; }
+        public string createdBY { get; set; }
+        public long modifiedID { get; set; }
+        public string modifiedBY { get; set; }
+        public DateTime created { get; set; }
+        public DateTime modified { get; set; }
+        public string street { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zipCode { get; set; }
+        public string country { get; set; }
+        public string skypeID { get; set; }
+        public string description { get; set; }
+        public bool emailOptOut { get; set; }
+        public string salutation { get; set; }
+        public string emailSec { get; set; }
+        public DateTime lastActivity { get; set; }
+        public string twitter { get; set; }
+*/
---
+++ Factories/ConectionFactory.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Web;
+using System.Text;
+using System.Net.Security;
+using System.Xml;
+
+namespace ZohoCrmConnector.Factories
+{
+    public abstract class ConectionFactory
+    {
+        protected static string zohocrmurl = "https://crm.zoho.com/crm/private/xml/";
+        protected string authToken { get; set; }
+
+        public ConectionFactory(string authToken)
+        {
+            this.authToken = authToken;
+        }
+        public XmlDocument getRecordById(string recordId)
+        {
+            string result = APIMethod("Leads", "getRecordById", recordId);//Change the id,method name, and module name here
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.LoadXml(result);
+            return xDoc;
+        }
+        public XmlDocument getMyRecords(string recordId)
+        {
+            string result = APIMethod("Leads", "getRecordById", recordId);//Change the id,method name, and module name here
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.LoadXml(result);
+            return xDoc;
+        }
+        private static String APIMethod(string modulename, string methodname, string recordId)
+        {
+            string uri = zohocrmurl + modulename + "/" + methodname + "?";
+            /* Append your parameters here */
+            string postContent = "scope=crmapi";
+            postContent = postContent + "&authtoken=b447d78308ef09d3bb3da3849da90c71";//Give your authtoken
+            if (methodname.Equals("insertRecords") || methodname.Equals("updateRecords"))
+            {
+                postContent = postContent + "&xmlData=" + HttpUtility.UrlEncode("Your CompanyHannahSmithtesting@testing.com");
+            }
+            if (methodname.Equals("updateRecords") || methodname.Equals("deleteRecords") || methodname.Equals("getRecordById"))
+            {
+                postContent = postContent + "&id=" + recordId;
+            }
+            string result = AccessCRM(uri, postContent);
+            Console.WriteLine(uri + postContent);
+            return result;
+        }
+        private static string AccessCRM(string url, string postcontent)
+        {
+            WebRequest request = WebRequest.Create(url);
+            request.Method = "POST";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postcontent);
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            WebResponse response = request.GetResponse();
+            dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            return responseFromServer;
+        }
+    }
+}
